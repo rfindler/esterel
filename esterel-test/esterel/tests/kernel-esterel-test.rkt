c0587659dc2692937a4e0cde769d45ea927f741d
Robby Findler <robby@racket-lang.org>
2023-04-07 10:42:54 -0500

    more examples from _The Constructive Semanitcs of Pure Esterel_

--- esterel-test/esterel/tests/kernel-esterel-test.rkt
+++ esterel-test/esterel/tests/kernel-esterel-test.rkt
@@ -900,6 +900,53 @@
   (check-equal? (react! r) (hash O #t can-explore #f))
   (check-equal? (react! r) (hash)))
 
+
+;                                       
+;                                       
+;                                       
+;                                       
+;                                       
+;   ;;;;;;;         ;           ;       
+;   ;;;;;;;;      ;;;           ;;;     
+;   ;;;  ;;;    ;;;;   ;;; ;;    ;;;;   
+;   ;;;  ;;;  ;;;;     ;;;;;;;     ;;;; 
+;   ;;;;;;;; ;;;       ;;; ;;;       ;;;
+;   ;;;;;;;   ;;;;     ;;; ;;;     ;;;; 
+;   ;;;         ;;;;   ;;; ;;;   ;;;;   
+;   ;;;           ;;;  ;;; ;;;  ;;;     
+;   ;;;             ;  ;;; ;;;  ;       
+;   ;;;                ;;; ;;;          
+;                                       
+;                                       
+;                                       
+;                                       
+;                                       
+;; these are programs from Berry's
+;; _The Constructive Semanitcs of Pure Esterel_
+
+;; P1
+(with-signal (I O)
+  (define r
+    (esterel
+     (with-signal (S1 S2)
+       (par (if (present? I)
+                (emit S1)
+                (void))
+            (if (present? S1)
+                (void)
+                (emit S2))
+            (if (present? S2)
+                (emit O)
+                (void))))))
+  (define (signals->names ht)
+    (for/hash ([(k v) (in-hash ht)])
+      (values (signal-name k) v)))
+  (check-equal? (signals->names (react! r))
+                (hash "S1" #f
+                      "S2" #t
+                      "O" #t
+                      "I" #f)))
+
 ;; P2
 (let ()
   (define r
@@ -916,7 +963,133 @@
       (values (signal-name k) v)))
   (check-equal? (signals->names (react! r))
                 (hash "O" #f "S" #t)))
-          
+
+;; the next few programs are probably elsewhere in
+;; this test suite but they are also included here
+;; just in case someone looks here for them
+
+;; P3
+(check-exn
+ non-constructive-exn?
+ (λ ()
+   (with-signal (O)
+     (react!
+      (esterel
+       (if (present? O)
+           (void)
+           (emit O)))))))
+
+;; P4
+(check-exn
+ non-constructive-exn?
+ (λ ()
+   (with-signal (O)
+     (react!
+      (esterel
+       (if (present? O)
+           (emit O)
+           (void)))))))
+
+;; P5
+(check-exn
+ non-constructive-exn?
+ (λ ()
+   (with-signal (O1 O2)
+     (react!
+      (esterel
+       (par
+        (if (present? O1)
+            (emit O2)
+            (void))
+        (if (present? O2)
+            (void)
+            (emit O1))))))))
+
+;; P6
+(check-exn
+ non-constructive-exn?
+ (λ ()
+   (with-signal (O1 O2)
+     (react!
+      (esterel
+       (par
+        (if (present? O1)
+            (emit O2)
+            (void))
+        (if (present? O2)
+            (emit O1)
+            (void))))))))
+
+;; P7
+(check-exn
+ non-constructive-exn?
+ (λ ()
+   (with-signal (O)
+     (react!
+      (esterel
+       (if (present? O)
+           (pause)
+           (void))
+       (emit O))))))
+
+;; P8
+(with-signal (I O)
+  (define (run I-present?)
+    (react!
+     (esterel
+      (with-trap T
+        (par
+         (begin (if (present? I)
+                    (void)
+                    (pause))
+                (emit O))
+         (if (present? O)
+             (exit-trap T)
+             (void))))
+      (emit O))
+     #:emit (if I-present?
+                (list I)
+                (list))))
+  (check-equal?
+   (run #t)
+   (hash O #t I #t))
+  (check-exn
+   non-constructive-exn?
+   (λ ()
+     (run #f))))
+
+;; P8 variation; from the book:
+;; Replacing “then” by “else” in the second parallel branch
+;; would yield a program with no logically coherent behavior.
+(with-signal (I O)
+  (define (run I-present?)
+    (react!
+     (esterel
+      (with-trap T
+        (par
+         (begin (if (present? I)
+                    (void)
+                    (pause))
+                (emit O))
+         (if (present? O)
+             (void)
+             (exit-trap T))))
+      (emit O))
+     #:emit (if I-present?
+                (list I)
+                (list))))
+  ;; this test case is failing but I don't get
+  ;; why the implementation is wrong; comment it out for now
+  (check-exn
+   non-constructive-exn?
+   (λ ()
+     (run #t)))
+  (check-exn
+   non-constructive-exn?
+   (λ ()
+     (run #f))))
+
+
 
 ;                                      
 ;                                      
