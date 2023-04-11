8cfb6427483339f4529f6ea8b0e3d36e444284bc
Robby Findler <robby@racket-lang.org>
2023-04-11 08:56:33 -0500

    getting started trying to understand value carrying signals by implementing must and can

---
+++ esterel-redex/must-can.rkt
@@ -0,0 +1,167 @@
+#lang racket
+(require redex/reduction-semantics)
+(define-language L
+  (p q ::=
+     (! s) (? s p q) (s ⊃ p)
+     (seq p q) (p *) (par p q)
+     (trap p) k
+     (p \\ s))
+  (k ::= natural)
+  (s ::= variable-not-otherwise-mentioned)
+
+  (fn ::= Must Can)
+  (Can ::= Can+ Can⊥)
+  (E ::= · (s = B⊥ E))
+  (B⊥ ::= tt ff ⊥)
+  (B ::= tt ff)
+  (set ::= · (any set))
+  (S ::= · (s S))
+  (K ::= · (k K))
+  (R ::= (Pr S K)))
+
+(define-metafunction L
+  mc_s : fn p E -> S
+  [(mc_s fn p E) S (where (Pr S K) (mc fn p E))])
+
+(define-metafunction L
+  mc_k : fn p E -> K
+  [(mc_k fn p E) K (where (Pr S K) (mc fn p E))])
+
+(define-metafunction L
+  mc : fn p E -> R
+  [(mc fn k E) (Pr · (k ·))]
+  
+  [(mc fn (! s) E) (Pr (s ·) (0 ·))]
+  
+  [(mc fn (? s p q) E R)
+   (mc fn p E R)
+   (where tt (lookup s E))]
+  
+  [(mc fn (? s p q) E R)
+   (mc fn q E R)
+   (where ff (lookup s E))]
+
+  [(mc Must (? s p q) E)
+   (Pr (∪ (mc_s fn p E)
+          (mc_s fn q E))
+       (∪ (mc_k fn p E)
+          (mc_k fn q E)))]
+
+  [(mc Can (? s p q) E)
+   (Pr · ·)]
+
+  [(mc fn (s ⊃ p))
+   (mc fn p)]
+
+  [(mc fn (seq p q) E)
+   (mc fn p E)
+   (where #false (∈ 0 (mc_k fn p E)))]
+
+  [(mc fn (seq p q) E)
+   (Pr (∪ (mc_s fn p E) (mc_s fn_2 q E))
+       (∪ (set-subtract (mc_k fn p E) 0)
+          (mc_s fn_2 q E)))
+   (where fn_2 (pickfn-seq fn p E))]
+
+  [(mc fn (p *) E) (mc fn p E)]
+
+  [(mc fn (par p q) E)
+   (Pr (∪ (mc_s fn p E) (mc_s fn q E))
+       (max (mc_k fn p E) (mc_k fn q E)))]
+
+  [(mc fn (trap p) E)
+   (Pr (mc_s fn p E)
+       (↓ (mc_k fn p E)))]
+
+  [(mc Must (p \\ s) E)
+   (mc Must p (extend s tt E))
+   (where #true (∈ s (fn_s Must p (extend s ⊥ E))))]
+
+  [(mc Must (p \\ s) E)
+   (remove-s (mc Must p (extend s ff E)) s)
+   (where #true (∈ s (fn_s Must p (extend s ⊥ E))))]
+
+  [(mc Must (p \\ s) E)
+   (remove-s (mc Must p (extend s ff E)) s)
+   (where #false (∈ s (fn_s Can+ p (extend s ⊥ E))))]
+
+  [(mc Must (p \\ s) E)
+   (remove-s (mc Must p (extend s ⊥ E)) s)]
+
+  [(mc Can+ (p \\ s) E)
+   (remove-s (mc Can+ p (extend s tt E)) s)
+   (where #true (∈ s (fn_s Must p (extend s ⊥ E))))])
+
+
+(define-metafunction L
+  Max : K K -> K
+  )
+
+(define-metafunction L
+  ↓ : K -> K
+  )
+
+(define-metafunction L
+  pickfn-seq : fn p E -> fn
+  [(pickfn-seq Must p E) Must]
+  [(pickfn-seq + p E)
+   +
+   (where #true (∈ 0 (mc Must p E)))]
+  [(pickfn-seq fn p E) ⊥])
+
+(define-metafunction L  
+  ∪ : set set -> set
+  [(∪ · set) set]
+  [(∪ (any set_1) set_2) (any (∪ set_1 (set- set_2 any)))])
+
+(define-metafunction L
+  set- : set any -> set
+  [(set- · any) ·]
+  [(set- (any set) any) set]
+  [(set- (any_1 set) any_2) (any_1 (set- set any_2))])
+
+(define-metafunction L
+  ∈ : any set -> boolean
+  [(∈ any ·) #false]
+  [(∈ any (any set)) #true]
+  [(∈ any_1 (any_2 set)) (∈ any_1 set)])
+
+(module+ test
+  (test-equal (term (set- · 1)) (term ·))
+  (test-equal (term (set- (1 ·) 1)) (term ·))
+  (test-equal (term (set- (2 (1 ·)) 1)) (term (2 ·)))
+  (test-equal (term (set- (1 (2 ·)) 1)) (term (2 ·)))
+  (test-equal (term (∪ · ·)) (term ·))
+  (test-equal (term (∪ (1 ·) ·)) (term (1 ·)))
+  (test-equal (term (∪ · (1 ·))) (term (1 ·)))
+  (test-equal (term (∪ (1 ·) (1 ·))) (term (1 ·)))
+  (test-equal (term (∪ (2 ·) (1 ·))) (term (2 (1 ·))))
+  (test-equal (term (∪ (1 ·) (2 ·))) (term (1 (2 ·))))
+  (test-equal (term (∪ (3 (2 (1 ·))) (1 (2 (3 ·))))) (term (3 (2 (1 ·)))))
+  (test-equal (term (∈ 1 (3 (2 (1 ·))))) #true)
+  (test-equal (term (∈ 2 (3 (2 (1 ·))))) #true)
+  (test-equal (term (∈ 3 (3 (2 (1 ·))))) #true)
+  (test-equal (term (∈ 4 (3 (2 (1 ·))))) #false))
+
+(define-metafunction L
+  remove-s : R -> R
+  [(remove-s (Pr S K) s) (Pr (set- S s) K)])
+
+(define-metafunction L
+  lookup : s E -> B⊥
+  [(lookup s (s = B⊥ E))
+   B⊥]
+  [(lookup s_1 (s_2 = B⊥ E))
+   (lookup s_1 E)])
+
+(define-metafunction L
+  extend : s B⊥ E -> E
+  [(extend s B⊥ E) (s = B⊥ E)])
+
+(module+ test
+  (test-equal (term (lookup s1 (extend s1 tt (extend s2 ff (extend s3 ⊥ ·)))))
+              (term tt))
+  (test-equal (term (lookup s2 (extend s1 tt (extend s2 ff (extend s3 ⊥ ·)))))
+              (term ff))
+  (test-equal (term (lookup s3 (extend s1 tt (extend s2 ff (extend s3 ⊥ ·)))))
+              (term ⊥)))
