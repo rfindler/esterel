91a6c2da4e202d6d58f9ec9652ca1bb2cc456f4e
Robby Findler <robby@racket-lang.org>
2023-04-11 15:15:26 -0500

    finish up must-can cases and add a few tests

--- esterel-redex/must-can.rkt
+++ esterel-redex/must-can.rkt
@@ -20,12 +20,12 @@
   (R ::= (Pr S K)))
 
 (define-metafunction L
-  mc_s : fn p E -> S
-  [(mc_s fn p E) S (where (Pr S K) (mc fn p E))])
+  mc-s : fn p E -> S
+  [(mc-s fn p E) S (where (Pr S K) (mc fn p E))])
 
 (define-metafunction L
-  mc_k : fn p E -> K
-  [(mc_k fn p E) K (where (Pr S K) (mc fn p E))])
+  mc-k : fn p E -> K
+  [(mc-k fn p E) K (where (Pr S K) (mc fn p E))])
 
 (define-metafunction L
   mc : fn p E -> R
@@ -42,72 +42,181 @@
    (where ff (lookup s E))]
 
   [(mc Must (? s p q) E)
-   (Pr (∪ (mc_s fn p E)
-          (mc_s fn q E))
-       (∪ (mc_k fn p E)
-          (mc_k fn q E)))]
+   (Pr · ·)]
 
   [(mc Can (? s p q) E)
-   (Pr · ·)]
+   (Pr (∪ (mc-s Must p E)
+          (mc-s Must q E))
+       (∪ (mc-k Must p E)
+          (mc-k Must q E)))]
 
   [(mc fn (s ⊃ p))
    (mc fn p)]
 
   [(mc fn (seq p q) E)
    (mc fn p E)
-   (where #false (∈ 0 (mc_k fn p E)))]
+   (where #false (∈ 0 (mc-k fn p E)))]
 
   [(mc fn (seq p q) E)
-   (Pr (∪ (mc_s fn p E) (mc_s fn_2 q E))
-       (∪ (set-subtract (mc_k fn p E) 0)
-          (mc_s fn_2 q E)))
+   (Pr (∪ (mc-s fn p E) (mc-s fn_2 q E))
+       (∪ (set- (mc-k fn p E) 0)
+          (mc-k fn_2 q E)))
    (where fn_2 (pickfn-seq fn p E))]
 
   [(mc fn (p *) E) (mc fn p E)]
 
   [(mc fn (par p q) E)
-   (Pr (∪ (mc_s fn p E) (mc_s fn q E))
-       (max (mc_k fn p E) (mc_k fn q E)))]
+   (Pr (∪ (mc-s fn p E) (mc-s fn q E))
+       (Max (mc-k fn p E) (mc-k fn q E)))]
 
   [(mc fn (trap p) E)
-   (Pr (mc_s fn p E)
-       (↓ (mc_k fn p E)))]
-
-  [(mc Must (p \\ s) E)
-   (mc Must p (extend s tt E))
-   (where #true (∈ s (fn_s Must p (extend s ⊥ E))))]
+   (Pr (mc-s fn p E)
+       (↓ (mc-k fn p E)))]
 
   [(mc Must (p \\ s) E)
    (remove-s (mc Must p (extend s ff E)) s)
-   (where #true (∈ s (fn_s Must p (extend s ⊥ E))))]
+   (where #true (∈ s (mc-s Must p (extend s ⊥ E))))]
 
   [(mc Must (p \\ s) E)
    (remove-s (mc Must p (extend s ff E)) s)
-   (where #false (∈ s (fn_s Can+ p (extend s ⊥ E))))]
+   (where #false (∈ s (mc-s Can+ p (extend s ⊥ E))))]
 
   [(mc Must (p \\ s) E)
    (remove-s (mc Must p (extend s ⊥ E)) s)]
 
   [(mc Can+ (p \\ s) E)
    (remove-s (mc Can+ p (extend s tt E)) s)
-   (where #true (∈ s (fn_s Must p (extend s ⊥ E))))])
+   (where #true (∈ s (mc-s Must p (extend s ⊥ E))))]
+
+  [(mc Can (p \\ s) E)
+   (remove-s (mc Can p (extend s ff E)) s)
+   (where #false (∈ s (mc-s Can p (extend s ⊥ E))))]
+
+  [(mc Can (p \\ s) E)
+   (remove-s (mc Can p (extend s ⊥ E)) s)]
+
+  )
+
+(define-judgment-form L
+  mc : fn p E -> R
+  [(mc fn k E) (Pr · (k ·))]
+  
+  [(mc fn (! s) E) (Pr (s ·) (0 ·))]
+  
+  [(mc fn (? s p q) E R)
+   (mc fn p E R)
+   (where tt (lookup s E))]
+  
+  [(mc fn (? s p q) E R)
+   (mc fn q E R)
+   (where ff (lookup s E))]
+
+  [(mc Must (? s p q) E)
+   (Pr · ·)]
+
+  [(mc Can (? s p q) E)
+   (Pr (∪ (mc-s Can p E)
+          (mc-s Can q E))
+       (∪ (mc-k Can p E)
+          (mc-k Can q E)))]
+
+  [(mc fn (s ⊃ p))
+   (mc fn p)]
+
+  [(mc fn (seq p q) E)
+   (mc fn p E)
+   (where #false (∈ 0 (mc-k fn p E)))]
+
+  [(mc fn (seq p q) E)
+   (Pr (∪ (mc-s fn p E) (mc-s fn_2 q E))
+       (∪ (set- (mc-k fn p E) 0)
+          (mc-k fn_2 q E)))
+   (where fn_2 (pickfn-seq fn p E))]
+
+  [(mc fn (p *) E) (mc fn p E)]
+
+  [(mc fn (par p q) E)
+   (Pr (∪ (mc-s fn p E) (mc-s fn q E))
+       (Max (mc-k fn p E) (mc-k fn q E)))]
+
+  [(mc fn (trap p) E)
+   (Pr (mc-s fn p E)
+       (↓ (mc-k fn p E)))]
+
+  [(mc Must (p \\ s) E)
+   (remove-s (mc Must p (extend s ff E)) s)
+   (where #true (∈ s (mc-s Must p (extend s ⊥ E))))]
+
+  [(mc Must (p \\ s) E)
+   (remove-s (mc Must p (extend s ff E)) s)
+   (where #false (∈ s (mc-s Can+ p (extend s ⊥ E))))]
+
+  [(mc Must (p \\ s) E)
+   (remove-s (mc Must p (extend s ⊥ E)) s)]
+
+  [(mc Can+ (p \\ s) E)
+   (remove-s (mc Can+ p (extend s tt E)) s)
+   (where #true (∈ s (mc-s Must p (extend s ⊥ E))))]
+
+  [(mc Can (p \\ s) E)
+   (remove-s (mc Can p (extend s ff E)) s)
+   (where #false (∈ s (mc-s Can p (extend s ⊥ E))))]
+
+  [(mc Can (p \\ s) E)
+   (remove-s (mc Can p (extend s ⊥ E)) s)]
+
+  )
+
 
 
 (define-metafunction L
   Max : K K -> K
-  )
+  [(Max · K) ·]
+  [(Max K ·) ·]
+  [(Max (k K_1) K_2)
+   (∪ (Max-kK k K_2)
+      (Max K_1 K_2))])
+
+(define-metafunction L
+  Max-kK : k K -> K
+  [(Max-kK k ·) ·]
+  [(Max-kK k_1 (k_2 K)) (∪ (Max-kK k_1 K) ((Max-kk k_1 k_2) ·))])
+
+(define-metafunction L
+  Max-kk : k k -> k
+  [(Max-kk k_1 k_2) ,(max (term k_1) (term k_2))])
+
+(module+ test
+  (test-equal (term (Max · ·)) (term ·))
+  (test-equal (term (Max · (1 ·))) (term ·))
+  (test-equal (term (Max (1 ·) ·)) (term ·))
+  (test-equal (term (Max (1 ·) (2 ·))) (term (2 ·)))
+  (test-equal (term (Max (1 (2 (3 ·))) (2 (3 (4 ·))))) (term (4 (3 (2 ·)))))
+  (test-equal (term (Max (0 (2 (4 (6 ·)))) (1 (3 (5 ·))))) (term (5 (3 (1 (2 (4 (6 ·)))))))))
 
 (define-metafunction L
   ↓ : K -> K
-  )
+  [(↓ ·) ·]
+  [(↓ (k K)) ((↓k k) (↓ K))])
+
+(define-metafunction L
+  ↓k : k -> k
+  [(↓k 0) 0]
+  [(↓k 2) 0]
+  [(↓k 1) 1]
+  [(↓k k) ,(- (term k) 1)])
+
+(module+ test
+  (test-equal (term (↓ (0 (1 (2 (3 (4 ·)))))))
+              (term (0 (1 (0 (2 (3 ·))))))))
 
 (define-metafunction L
   pickfn-seq : fn p E -> fn
   [(pickfn-seq Must p E) Must]
-  [(pickfn-seq + p E)
-   +
-   (where #true (∈ 0 (mc Must p E)))]
-  [(pickfn-seq fn p E) ⊥])
+  [(pickfn-seq Can+ p E)
+   Can+
+   (where #true (∈ 0 (mc-k Must p E)))]
+  [(pickfn-seq fn p E) Can⊥])
 
 (define-metafunction L  
   ∪ : set set -> set
@@ -144,7 +253,7 @@
   (test-equal (term (∈ 4 (3 (2 (1 ·))))) #false))
 
 (define-metafunction L
-  remove-s : R -> R
+  remove-s : R s -> R
   [(remove-s (Pr S K) s) (Pr (set- S s) K)])
 
 (define-metafunction L
@@ -165,3 +274,36 @@
               (term ff))
   (test-equal (term (lookup s3 (extend s1 tt (extend s2 ff (extend s3 ⊥ ·)))))
               (term ⊥)))
+
+
+(module+ test
+  (test-equal (term (mc Must ((? s (! s) (! s)) \\ s) ·)) (term (Pr · ·)))
+  (test-equal (term (mc Can+ ((? s (! s) (! s)) \\ s) ·)) (term (Pr · (0 ·))))
+  (test-equal (term (mc Can⊥ ((? s (! s) (! s)) \\ s) ·)) (term (Pr · (0 ·))))
+  (test-equal (term (mc Must (? s (! s) (! s)) ·)) (term (Pr · ·)))
+  (test-equal (term (mc Can+ (? s (! s) (! s)) ·)) (term (Pr (s ·) (0 ·))))
+  (test-equal (term (mc Can⊥ (? s (! s) (! s)) ·)) (term (Pr (s ·) (0 ·))))
+  (test-equal (term (mc Must (par (! s) (! t)) ·)) (term (Pr (s (t ·)) (0 ·))))
+  (test-equal (term (mc Can+ (par (! s) (! t)) ·)) (term (Pr (s (t ·)) (0 ·))))
+  (test-equal (term (mc Can⊥ (par (! s) (! t)) ·)) (term (Pr (s (t ·)) (0 ·))))
+
+  (test-equal (term (mc Must (seq 1 2) ·)) (term (Pr · (1 ·))))
+  (test-equal (term (mc Can+ (seq 1 2) ·)) (term (Pr · (1 ·))))
+  (test-equal (term (mc Can⊥ (seq 1 2) ·)) (term (Pr · (1 ·))))
+  (test-equal (term (mc Must (seq 0 1) ·)) (term (Pr · (1 ·))))
+  (test-equal (term (mc Can+ (seq 0 1) ·)) (term (Pr · (1 ·))))
+  (test-equal (term (mc Can⊥ (seq 0 1) ·)) (term (Pr · (1 ·))))
+  )
+
+(current-traced-metafunctions '(mc))
+(define P8
+  (term
+   (seq (trap
+         (par
+          (seq (? I 0 1)
+               (! O))
+          (? O 0 2)))
+        (! O))))
+(term (mc Can+ ,P8 ·))
+(term (mc Can⊥ ,P8 ·))
+
